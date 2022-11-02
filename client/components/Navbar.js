2946105e9201a008023e6780b70f6a0928ec6072
ngkangting <benji.ngkt@gmail.com>
2022-11-03 00:04:52 +0800

    Change Name

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex-shrink-0 flex items-center">
                 <h4 className="font-mono text-xl text-greay font-bold hidden lg:block">
-                  KICKSTARTER
+                  FUNDSTARTER
                 </h4>
               </div>
               <div className="hidden sm:block sm:ml-6">
